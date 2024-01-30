f9a9025b110680088b6e00c0e03ae7d2d052bfc6
sho-neko <nittundoraemon@outlook.com>
2024-01-30 23:47:31 +0900

    Add aplusb.cpp to test/library_checker/sample

---
+++ test/library_checker/sample/aplusb.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+
+void solve(){
+    ll a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+}
+
+int main(){
+    int t=1;
+    rep(i,t)solve();
+    return 0;
+}
