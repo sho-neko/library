87b84f79d697edfa49e8d53a8bd72bd180faf830
sho-neko <nittundoraemon@outlook.com>
2024-02-11 20:31:56 +0900

    unionfind

---
+++ data_structure/unionfind.hpp
@@ -0,0 +1,17 @@
+#include "../my_template.hpp"
+struct UnionFind {
+    vector<int> data;
+    UnionFind(int size) : data(size, -1) {}
+    bool merge(int x, int y) {
+        x = root(x), y = root(y);
+        if(x != y) {
+            if(data[y] < data[x])
+                swap(x, y);
+            data[x] += data[y], data[y] = x;
+        }
+        return x != y;
+    }
+    bool same(int x, int y) { return root(x) == root(y); }
+    int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+    int size(int x) { return -data[root(x)]; }
+};
---
+++ test/library_checker/data_structure/unionfind.cpp
@@ -0,0 +1,13 @@
+#include "../../../data_structure/unionfind.hpp"
+#include "../../../my_template.hpp"
+int main() {
+    ll n, q;
+    cin >> n >> q;
+    UnionFind uf(n);
+    rep(i, q) {
+        ll t, u, v;
+        cin >> t >> u >> v;
+		if(t == 0) uf.merge(u, v);
+		else cout << uf.same(u, v) << endl;
+    }
+}
