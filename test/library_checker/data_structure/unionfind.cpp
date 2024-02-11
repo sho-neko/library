5516726769329113cee1f865cc2b177af8dd0831
sho-neko <nittundoraemon@outlook.com>
2024-02-11 20:48:20 +0900

    unionfind 入出力高速化

--- test/library_checker/data_structure/unionfind.cpp
+++ test/library_checker/data_structure/unionfind.cpp
@@ -1,14 +1,22 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 #include "../../../data_structure/unionfind.hpp"
 #include "../../../my_template.hpp"
-int main() {
+void solve() {
     ll n, q;
     cin >> n >> q;
     UnionFind uf(n);
     rep(i, q) {
         ll t, u, v;
         cin >> t >> u >> v;
-		if(t == 0) uf.merge(u, v);
-		else cout << uf.same(u, v) << endl;
+        if(t == 0)
+            uf.merge(u, v);
+        else
+            cout << uf.same(u, v) << "\n";
     }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    ll t = 1;
+    rep(i, t) solve();
 }
